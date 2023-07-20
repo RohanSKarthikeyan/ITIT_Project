c16c2e4662b0fe20475e8dcc47220893d7f603c8
RohanSKarthikeyan <rohankarthikeyan7@gmail.com>
2023-07-20 19:31:50 +0530

    nav and login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import './CSS/App.css';
+import Header from './Header';
+import Login from './Login';
+import {BrowserRouter as Router, Routes, Route, Switch, BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div className="App">
-      
+    <Routes>
+                <Route exact path="/" element={<Header/>} />
+                <Route exact path="/login" element={<Login/>} />
+     </Routes>           
     </div>
+    </BrowserRouter>
   );
 }
 
